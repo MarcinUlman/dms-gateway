d2c996075ca9d54c0eae31ab32e369362bf6e01f
Marcin Ulman <marcin@ulman@dev>
2019-09-28 09:44:04 +0200

    init gateway

---
+++ src/test/java/dev/ulman/dmsgateway/DmsGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.ulman.dmsgateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DmsGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
